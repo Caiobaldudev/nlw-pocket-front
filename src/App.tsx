20aa711e08138e6e9dc858a869fc821be51a98a1
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-11 17:46:59 -0300

    feat: create components & i radix

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import logo from './assets/logo.svg'
+import letsStart from './assets/ilustration.svg'
+import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="inorbit" />
+      <img src={letsStart} alt="inorbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 
+import './index.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
