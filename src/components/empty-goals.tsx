0fb9ddac97eee4cab1802521470bc8e51d3bb006
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-11 18:19:35 -0300

    feat: componentizing homepage and modal

---
+++ src/components/empty-goals.tsx
@@ -0,0 +1,23 @@
+import logo from '../assets/logo.svg'
+import letsStart from '../assets/ilustration.svg'
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="inorbit" />
+      <img src={letsStart} alt="inorbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
