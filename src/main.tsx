24406f2ea9bf3682b20ebf775062b7b86345ecba
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-11 16:17:20 -0300

    feat: i tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {App} from './app'
+import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
