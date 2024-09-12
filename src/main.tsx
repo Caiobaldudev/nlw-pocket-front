35659103ef46a7b8041fe37805cb9017b113b9e0
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-12 16:03:05 -0300

    feat: config tanstack index

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>
+  </QueryClientProvider>
 )
