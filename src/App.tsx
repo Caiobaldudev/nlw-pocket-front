bcbf4b5712575088989c6fbf969fdc92b5d419d6
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-12 16:04:26 -0300

    feat: implementation back to front

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-// import { EmptyGoals } from './components/empty-goals'
+import { EmptyGoals } from './components/empty-goals'
 import { Dialog } from './components/ui/dialog'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
+
   return (
     <Dialog>
-     {/* <EmptyGoals/> */}
-     <Summary/>
-      <CreateGoal/>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      <CreateGoal />
     </Dialog>
   )
 }
