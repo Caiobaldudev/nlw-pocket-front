bd8ab71dba4e10440429ac7fbcb71bbd9463711a
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-12 16:02:13 -0300

    feat: get summary response api

---
+++ src/http/get-summary.ts
@@ -0,0 +1,19 @@
+type SummaryRes = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
+
+export async function getSummary(): Promise<SummaryRes> {
+  const response = await fetch('http://localhost:3333/summary')
+  const data = await response.json()
+
+  return data.summary
+}
